9dc93465c862eab9b4870e4a0771106a51b7307b
retip94 <38405004+retip94@users.noreply.github.com>
2019-12-13 10:09:22 +0100

    finished part 2

--- 1_1/main.py
+++ 1_1/main.py
@@ -1,11 +1,26 @@
-f = open("input.txt", "r")
-input = f.read()
-modules_masses = input.split('\n')
-modules_masses = set(map(lambda x:int(x),modules_masses))
+class DayOne:
+    def __init__(self):
+        f = open("input.txt", "r")
+        input = f.read()
+        self.modules_masses = set(map(lambda x: int(x), input.split('\n')))
+        self.fuel = 0
 
-fuel_sum = 0
-for module_mass in modules_masses:
-    # int() will round down the result
-    fuel_sum += int(module_mass/3)-2
+    def part_one(self):
+        for module_mass in self.modules_masses:
+            # int() will round down the result
+            self.fuel += int(module_mass / 3) - 2
+        return self.fuel
 
-print(fuel_sum)
+    def part_two(self):
+        for module_mass in self.modules_masses:
+            additional_mass = int(module_mass / 3) - 2
+            while additional_mass > 0:
+                self.fuel += additional_mass
+                additional_mass = int(additional_mass / 3) - 2
+        return self.fuel
+
+
+answers = DayOne()
+print("answer to part1 is: ", answers.part_one())
+answers = DayOne()
+print("answer to part2 is: ", answers.part_two())
